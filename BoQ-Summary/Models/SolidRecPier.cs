89925d4a0c01015cabe45d0d6113ab48bb594fab
bill hu <billhu0228@icloud.com>
2019-02-18 13:43:21 +0800

    1

---
+++ BoQ-Summary/Models/SolidRecPier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoQ_Summary.Models
+{
+    class SolidRecPier
+    {
+        public double LongDim
+        {
+            set; get;
+        }
+        public double TransDim
+        {
+            set; get;
+        }
+        public double Height
+        {
+            set; get;
+        }
+
+        /// <summary>
+        /// 输出
+        /// </summary>
+        /// <param name="dt"></param>
+        public void WriteData(ref DataTable dt)
+        {
+
+        }
+    }
+}
