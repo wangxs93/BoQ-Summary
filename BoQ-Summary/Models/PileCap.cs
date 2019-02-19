f083483e03afd133a565106bbf41a4ca494944e4
bill hu <billhu0228@icloud.com>
2019-02-19 10:51:17 +0800

    Revert "oo"
    This reverts commit 30ff277c6a50f21d1452a86e4e6b950fe1f4825c.

--- BoQ-Summary/Models/PileCap.cs
+++ BoQ-Summary/Models/PileCap.cs
@@ -7,57 +7,36 @@ using System.Threading.Tasks;
 
 namespace BoQ_Summary.Models
 {
-    public class PileCap:BaseModel//桩承台统计
+    public class PileCap:BaseModel
     {
-        public double LongDim//顺桥向长度
+        public double LongDim
         {
             set;get;
         }
-        public double TransDim//横桥向长度
+        public double TransDim
         {
             set;get;
         }
-        public double Height//承台高度
+        public double Height
         {
             set;get;
-        }
-        public double Rho//承台配筋率
-        {
-            set;get;
-        }
-        double Vol1//承台体积
-        {
-            get
-            {
-                return LongDim * TransDim * Height;
-            }
-        }
-        double Vol2//垫层体积
-        {
-            get
-            {
-                return (LongDim+2*0.1) * (TransDim+2*0.1) * 0.1;
-            }
         }
         /// <summary>
         /// 构造函数
         /// </summary>
-        public PileCap(double longDim,double transDim,double height,double r)
+        public PileCap(double a)
         {
-            LongDim=longDim;
-            TransDim=transDim;
-            Height=height;
-            Rho = r;
+
         }
+
+
         /// <summary>
         /// 输出
         /// </summary>
         /// <param name="dt"></param>
-        public void WriteData(ref DataTable dt,string br,int xmh_zj,int xmh_zjrebar)
+        public void WriteData(ref DataTable dt)
         {
-            WriteRcd(ref dt, br,"桩承台", "","","混凝土","",LongDim,Vol1, xmh_zj);
-            WriteRcd(ref dt, br,"承台垫层", "","","混凝土","",LongDim,Vol2, xmh_zj);
-            WriteRcd(ref dt, br, "桩承台", "", "", "钢筋", "", Rho* Vol1,LongDim, xmh_zjrebar);
+
         }
     }
 }
